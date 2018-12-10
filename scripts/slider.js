0a62f30ea95dfbfec2c14b237ceea071ca0c49ac
Tyler Paavola <tyler@tekniika.com>
2018-12-09 22:29:38 -0600

    Add hyperlinks and basic styling to slider
    Add blank placeholder pages for AWG services.
    Add hyperlinks to navigation bar.
    Add hyperlinks to AWG services slider for AWG services pages.
    Add some basic styling to AWG services slider.
    Move primary stylesheet (style.css) to 'styles' folder for directory
    structure consistency.

--- scripts/slider.js
+++ scripts/slider.js
@@ -2,23 +2,19 @@ $(document).ready(function ($) {
     $('.services-slider').slick({
         dots: false,
         arrows: false,
+        centerMode: true,
         infinite: true,
         speed: 500,
-        slidesToShow: 3,
         slidesToScroll: 1,
         autoplay: false,
-        responsive: [{
-            breakpoint: 600,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 1
-            }
-        },
+        variableWidth: true,
+        responsive: [
         {
-            breakpoint: 400,
+            breakpoint: 480,
             settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
+                variableWidth: false,
+                centerMode: false,
+                slidesToShow: 1
             }
         }]
     });
